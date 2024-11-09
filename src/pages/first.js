aa487ffc0653403f13cd1f7825a592ce1b76f5fa
kacper-godula <kacper.godula@schibsted.com>
2024-11-09 16:02:04 +0100

    add a bit of info

--- src/pages/first.js
+++ src/pages/first.js
@@ -2,6 +2,12 @@ export default function Page() {
   return (
     <div className='flex flex-col justify-center items-center h-screen p-10'>
       <p>
+        Wspomnijmy razem Adziczka. Być może nie był on najmądrzejszym, ale na
+        pewno był najbardziej kochanym z osiołków. Wszyscy go kochali, a on
+        tolerował wszystkich.
+      </p>
+      <p className='mt-10'>Ale czy na pewno...?</p>
+      <p className='mt-10'>
         Pierwsza wskazówka znajduje się tam, gdzie nasz kochany Adi stoczył
         nieludzkie batalie po pochłonięciu 2137 zeberek.
       </p>
